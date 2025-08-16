b477c497d4c2065388f9b5a3d8afa3090cc6d126
99hyeon <hyeon99@ajou.ac.kr>
2025-08-16 21:56:59 +0900

    [#1] chore: Spring Boot, React 스캐폴딩

---
+++ src/main/java/com/example/japtangjjigae/JaptangjjigaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.japtangjjigae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaptangjjigaeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaptangjjigaeApplication.class, args);
+    }
+
+}
